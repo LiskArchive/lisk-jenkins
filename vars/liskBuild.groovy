f9a84dbbfeb08cf96cf1cb681cc67d4e094c6bb3
Nazar Hussain <nazarhussain@gmail.com>
2018-05-08 17:21:10 +0200

    Add liskbuild as global function

---
+++ vars/liskBuild.groovy
@@ -0,0 +1,47 @@
+
+
+def call(body) {
+	def params = [:]
+
+	def liskBranch = params.branch
+	def buildNetwork = params.network
+
+	def liskDir = "/tmp/lisk-${liskBranch}-${Math.abs(new Random().nextInt() % 1000) + 1}"
+	def liskVersion = ""
+	def liskReleaseName = ""
+	def liskBuildDir = ""
+	def liskBuildBranch = ""
+
+	stage("Lisk Build ${liskBranch}") {
+		steps {
+			dir(liskDir) {
+				git url: "https://github.com/LiskHQ/lisk.git", branch: liskBranch
+
+				sh """
+				npm install
+				node ./node_modules/.bin/grunt release
+				jq -r '.version' config.json > ".lisk-version"
+				"""
+			}
+			script {
+				liskVersion = readFile("${liskDir}/.lisk-version").trim()
+
+				if(fileExists("${liskDirectory}/genesisBlock.json")) {
+					liskBuildBranch = "0.9.15"
+					liskReleaseName = "${liskVersion}.tar.gz"
+				} else {
+					liskBuildBranch = "1.0.0"
+					liskReleaseName = "lisk-${liskVersion}.tgz"
+				}
+				liskBuildDir = "/tmp/lisk-build-${liskBuildBranch}-${Math.abs(new Random().nextInt() % 1000) + 1}"
+			}
+			dir(liskBuildDir) {
+				git url: "https://github.com/LiskHQ/lisk-build.git", branch: liskBuildBranch
+				sh """
+				cp "${liskDir}/release/${liskReleaseName}" "src/"
+				bash build.sh -n "${buildNetwork}" -v "${liskVersion}"
+				"""
+			}
+		}
+	}
+}
