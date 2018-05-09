04b2e1776f69d1a0fae4d74ab84064049391ae02
Nazar Hussain <nazarhussain@gmail.com>
2018-05-09 12:33:44 +0200

    Add support to detect pre 1.0.0 version

--- vars/liskBuild.groovy
+++ vars/liskBuild.groovy
@@ -10,6 +10,7 @@ def call(Map params) {
 
 	def liskBuildDir = ""
 	def liskBuildBranch = ""
+	def	latestVersion = false 
 
 	dir(liskDir) {
 		git url: "https://github.com/LiskHQ/lisk.git", branch: liskBranch
@@ -26,9 +27,11 @@ def call(Map params) {
 		if(fileExists("${liskDir}/genesisBlock.json")) {
 			liskBuildBranch = "0.9.15"
 			liskReleaseName = "${liskVersion}.tar.gz"
+			latestVersion = false
 		} else {
 			liskBuildBranch = "1.0.0"
 			liskReleaseName = "lisk-${liskVersion}.tgz"
+			latestVersion = true
 		}
 		liskBuildDir = "/tmp/lisk-build-${liskBuildBranch}-${Math.abs(new Random().nextInt() % 1000) + 1}"
 	}
@@ -40,5 +43,5 @@ def call(Map params) {
 		"""
 	}
 
-	return [version: liskVersion, network: buildNetwork, file: "${liskBuildDir}/release/lisk-${liskVersion}-Linux-x86_64.tar.gz"]
+	return [version: liskVersion, latestVersion: latestVersion, network: buildNetwork, file: "${liskBuildDir}/release/lisk-${liskVersion}-Linux-x86_64.tar.gz"]
 }
