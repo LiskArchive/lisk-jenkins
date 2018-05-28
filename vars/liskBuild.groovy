48567202a17500c88455bc8511ac01c67afdb7a7
Nazar Hussain <nazar@lightcurve.io>
2018-05-28 15:42:05 +0200

    Update build path to use workspace directory

--- vars/liskBuild.groovy
+++ vars/liskBuild.groovy
@@ -5,12 +5,13 @@ def call(Map params) {
 	def buildNetwork = params.network
 	def useTestConfig = params.useTestConfig || false
 
-	def liskDir = "/tmp/lisk-${liskBranch}-${Math.abs(new Random().nextInt() % 1000) + 1}"
+	def liskDir = "lisk-${liskBranch}-${Math.abs(new Random().nextInt() % 1000) + 1}"
 	def liskVersion = ""
 	def liskReleaseName = ""
 
 	def liskBuildDir = ""
 	def liskBuildBranch = ""
+	def latestVersion = false
 
 	dir(liskDir) {
 		git url: "https://github.com/LiskHQ/lisk.git", branch: liskBranch
@@ -47,11 +48,13 @@ def call(Map params) {
 		if(fileExists("${liskDir}/genesisBlock.json")) {
 			liskBuildBranch = "0.9.15"
 			liskReleaseName = "${liskVersion}.tar.gz"
+			latestVersion = false
 		} else {
 			liskBuildBranch = "1.0.0"
 			liskReleaseName = "lisk-${liskVersion}.tgz"
+			latestVersion = true
 		}
-		liskBuildDir = "/tmp/lisk-build-${liskBuildBranch}-${Math.abs(new Random().nextInt() % 1000) + 1}"
+		liskBuildDir = "lisk-build-${liskBuildBranch}-${Math.abs(new Random().nextInt() % 1000) + 1}"
 	}
 	dir(liskBuildDir) {
 		git url: "https://github.com/LiskHQ/lisk-build.git", branch: liskBuildBranch
@@ -61,5 +64,5 @@ def call(Map params) {
 		"""
 	}
 
-	return [version: liskVersion, network: buildNetwork, file: "${liskBuildDir}/release/lisk-Linux-x86_64.tar.gz"]
+	return [version: liskVersion, latestVersion: latestVersion, network: buildNetwork, file: "${liskBuildDir}/release/lisk-Linux-x86_64.tar.gz"]
 }
